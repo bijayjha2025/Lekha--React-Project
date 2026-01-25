0e48005306d62d2ce560d834e264370d28f30043
Bijay Jha <bijayjha12345@gmail.com>
2026-01-25 21:38:40 +0545

    using sidebar, add note, and image use

--- lekha/src/App.jsx
+++ lekha/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, use } from 'react';
+import { useState, useEffect } from 'react';
 import { Plus, Trash2 } from 'lucide-react';
 
 function App() {
@@ -52,7 +52,7 @@ function App() {
     if(!activeNoteId) return;
 
     setNotes(notes.map(note =>
-      note.id === activeNoteId ? { ...note, title: currentTitle, content: currentContent, updatedAt: new Date().toLocaleDateString()() } : note ));
+      note.id === activeNoteId ? { ...note, title: currentTitle, content: currentContent, updatedAt: new Date().toLocaleDateString() } : note ));
   }
 
   const deleteNote = (id, e) => {
@@ -83,30 +83,61 @@ function App() {
   const charCount = currentContent.length;
 
   return(
-  <div className='min-h-screen bg-gray-100 p-8'>
-    <div className='max-w-2xl mx-auto bg-white rounded-lg shadow-lg p-6'>
-      <div className='flex items-center justify-between mb-4'>
-      <h1 className='text-3xl font-bold text-gray-800'>My note</h1>
+  <div className='flex h-screen bg-gray-100'>
+   <div className='w-64 bg-white border-r border-gray-200 flex flex-col'>
+    <div className='p-4 border-b border-gray-200'>
+     <h1 className='text-xl font-bold text-gray-800 mb-3'>My Notes</h1>
 
-      {
-        lastSaved && (
-          <p className='text-sm text-gray-500'>Last Saved: {lastSaved}</p>
-      )}
-      </div>
-
-      <textarea value={note} onChange={(e) => setNote(e.target.value)} placeholder='Start tracking your note' className='w-full h-64 p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#bff542] resize-none' />
-
-      <div className='flex items-center justify-between mt-4'>
-        <p className=' text-sm text-gray-600'>{charCount} character{charCount !== 1 ? 's' : ''}</p>
-
-        <div className='flex gap-2'>
-          <button onClick={clearNote} className='mt-4 px-6 py-2 bg-[#fa9e9b] text-black rounded-lg font-semibold hover:bg-[#fa7f7a] transition-colors cursor-pointer'>✖️ Clear</button>
-          
-          <button onClick={saveNote} className='mt-4 px-6 py-2 bg-[#aaf542] text-black font-semibold rounded-lg hover:bg-[#23e83a] transition-colors cursor-pointer'>💾 Save</button>
-        </div>
-      </div>
+     <button onClick={createNewNote} className='w-full flex items-center justify-center gap-2 px-4 py-2 bg-[#71f022] text-black rounded-lg hover:bg-[#0dd417] transition-colors cursor-pointer'><Plus size={18} /> New Note</button>
     </div>
+
+    <div className='flex-1 overflow-y-auto'>
+     {notes.length === 0 ? (
+      <p className='p-4 text-gray-600'>No notes yet. Create one to get started!</p>) : (
+            
+      notes.map(note => (
+      <div key={note.id} onClick={() => selectNote(note)} className={`p-4 border-b border-gray-200 cursor-pointer hover:bg-gray-50 ${note.id === activeNoteId ? 'bg-gray-200' : ''}`}>
+       <div className='flex justify-between items-center mb-1'>
+        <h3>{note.title || 'Untitled'}</h3>
+
+        <button onClick={(e) => deleteNote(note.id, e)} className='text-red-500 hover:text-red-700'><Trash2 size={16} /></button>
+       </div>
+      <p className='text-xs text-gray-500'>{new Date(note.createdAt).toLocaleDateString()}</p>
+     </div>
+    ))
+    )}
+   </div>
   </div>
+
+  <div className='flex-1 flex flex-col'>
+    {activeNoteId ? (
+     <>
+      <div className='p-4 border-b border-gray-200'>
+       <input type='text' value={currentTitle} onChange={(e) => setCurrentTitle(e.target.value)} onBlur={saveCurrentNote} placeholder='Note Title...' className='w-full p-4 border-none text-gray-800 text-2xl font-bold focus:outline-none focus:ring-2 focus:ring-[#bff542]' />
+      </div>
+
+      <div className="flex-1 p-6 overflow-y-auto">
+       <textarea value={currentContent} onChange={(e) => setCurrentContent(e.target.value)} onBlur={saveCurrentNote}placeholder="Start typing your note..." className="w-full h-full p-4 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-[#bff542] resize-none" />
+      </div>
+      
+      <div className="bg-white border-t border-gray-200 p-4">
+       <div className="flex items-center justify-between max-w-4xl mx-auto">
+        <p className="text-sm text-gray-600">{charCount} character{charCount !== 1 ? 's' : ''}</p>
+        <button onClick={saveCurrentNote} className="px-6 py-2 bg-[#71f022] text-black rounded-lg hover:bg-[#0dd417] transition-colors cursor-pointer">Save</button>
+       </div>
+      </div>
+      </>
+
+      ) : (
+       <div className="flex-1 flex items-center justify-center">
+        <div className="text-center">
+         <p className="text-gray-500 text-lg mb-4">No note selected</p>
+         <button onClick={createNewNote} className="flex items-center gap-2 px-6 py-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition-colors mx-auto" ><Plus size={20} />Create Your First Note</button>
+        </div>
+        </div>
+       )}
+     </div>
+    </div>
   );
 }
 
